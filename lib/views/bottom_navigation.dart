f922836acfdbe57933d79737692687ca02bd42b5
syedmahiuddin <syedmahiuddin01@gmail.com>
2024-08-17 20:08:57 +0600

    dark bottom_navigation.dart

--- lib/views/bottom_navigation.dart
+++ lib/views/bottom_navigation.dart
@@ -4,6 +4,7 @@ import 'package:bankdemo/views/send_money.dart';
 import 'package:bankdemo/views/transfer_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../customs/color_helper.dart';
 
@@ -34,34 +35,48 @@ class _BottomNavScreenState extends State<BottomNavScreen> {
       body: Center(
         child: _screens[_selectedIndex],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: ColorHelper.mainFontColor,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined, color: ColorHelper.themeGreyColor),
-            activeIcon: Icon(Icons.home_outlined, color: ColorHelper.secondaryFontColor),
-            label: 'Home',
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: ColorHelper.themeBackColor,
+          border: Border(
+            top: BorderSide(
+              color: ColorHelper.themeGreyColor.withOpacity(0.5), // Color of the top border
+              width: 1.0, // Thickness of the top border
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.credit_card_rounded, color: ColorHelper.themeGreyColor),
-            activeIcon: Icon(Icons.credit_card_rounded, color: ColorHelper.secondaryFontColor),
-            label: 'Card',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group_add_outlined, color: ColorHelper.themeGreyColor),
-            activeIcon: Icon(Icons.group_add_outlined, color: ColorHelper.secondaryFontColor),
-            label: 'Recipients',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.repeat, color: ColorHelper.themeGreyColor),
-            activeIcon: Icon(Icons.repeat, color: ColorHelper.secondaryFontColor),
-            label: 'Payments',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: ColorHelper.secondaryFontColor,
-        unselectedItemColor: ColorHelper.themeGreyColor,
-        onTap: _onItemTapped,
+
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: ColorHelper.themeBackColor, // Set background color
+          elevation: 0, // Remove elevation to prevent shadow that may cause a white background
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined, color: ColorHelper.themeGreyColor),
+              activeIcon: Icon(Icons.home_outlined, color: ColorHelper.secondaryFontColor),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card_rounded, color: ColorHelper.themeGreyColor),
+              activeIcon: Icon(Icons.credit_card_rounded, color: ColorHelper.secondaryFontColor),
+              label: 'Card',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.group_add_outlined, color: ColorHelper.themeGreyColor),
+              activeIcon: Icon(Icons.group_add_outlined, color: ColorHelper.secondaryFontColor),
+              label: 'Recipients',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.repeat, color: ColorHelper.themeGreyColor),
+              activeIcon: Icon(Icons.repeat, color: ColorHelper.secondaryFontColor),
+              label: 'Payments',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: ColorHelper.secondaryFontColor,
+          unselectedItemColor: ColorHelper.themeGreyColor,
+          onTap: _onItemTapped,
+        ),
       ),
       backgroundColor: ColorHelper.themeBackColor,
     );
